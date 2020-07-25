c0d54988e09932731d9553c7234da7a22965e829
Scott H <telly@TELLY>
2020-07-26 00:49:04 +1000

    updates to SLB

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # sage-linux-backup
-Linux file backup (tar) with logging and cleanup of old backups
-
-# Purpose
+Linux file backup (tar) with logging and cleanup of old backups.
 
 # Features
+* full tar backup to remote mount location
+* exclude folders
+* cleanup backups
+* email upon success / failure
 
 # Prerequisite
 Install:
@@ -17,4 +19,26 @@ Configure:
 * /etc/ssmtp/revaliases
 - i.e. <user>:<ssmpt email address>:<ssmpt IP>:<ssmtp port>
 
+# Setup
+* download scripts
+* cp slb-config.sh as
+* confirm / check scripts have execute permissions
+* add scripts to cron
+
+# Change log
+* 1.0 04-05-2017
+** first release
+* 1.1 15-10-2018
+** check mount and email if mount fails
+* 1.2 19-11-2018
+** update DESDIR with HOST instead of hard coded hostname
+** add cleanup post re-mount + make hostname a variable
+* 1.3 22-11-2018
+** updated email to include some log output
+* 1.4 22-01-2019
+** added directory output to email
+* 1.5 26-07-2020
+** cleaned up for git
+** merged backup and cleanup into single script with functions
+ 
 # END
--- sage-linux-backup.sh
+++ sage-linux-backup.sh
@@ -3,38 +3,88 @@
 # sage-linux-backup
 
 #START
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-HOST=`hostname`                                     # get hostname
-DATE=`date +%Y%m%d`                                 # get date
-SDATETIME=`date "+%Y-%m-%d %H:%M:%S"`               # get date and time
-FILENAME=$HOST-backup-$DATE.tar.gz                  # set tar.gz filename
-LOGFILENAME=$HOST-backup-$DATE.log                  # set log filename
-EXCLIST=$SCRIPTDIR/sage-linux-backup-exclude.list   # exclude list
-SRCDIR=/                                            # set source directory
-DESMNT=MOUNT FROM CONFIG                            # mount point
-DESDIR=$DESMNT/$HOST                                # set destination directory
-EMAIL=EMAIL FROM CONFIG                             # set email to alert
 
-if grep -qs "$MOUNT" /proc/mounts; then
-  # MOUNT mounted - proceed with backup
-  LOGVAR+=$(echo -e "Backup started at $SDATETIME" | tee -a $DESDIR/$LOGFILENAME)'\n'
-  LOGVAR+=$(echo -e "Backup $HOST $SRCDIR to $DESDIR" | tee -a $DESDIR/$LOGFILENAME)'\n'
-  # run backup
-  tar -cpzvf $DESDIR/$FILENAME --directory=$SRCDIR --exclude-from $EXCLIST . &>> $DESDIR/$LOGFILENAME
-  EDATETIME=`date "+%Y-%m-%d %H:%M:%S"`             # get date and time
-  DURATION=`datediff -f "%H hours and %M minutes and %S seconds" "$SDATETIME" "$EDATETIME"`
-  DESDIRLS=`find $DESDIR/* -maxdepth 1 -printf '%CY-%Cm-%Cd - %f  \t- %s\n' | numfmt --to=iec --suffix=B --field 5 --format='%3.3f' | sed 's/.000//'`
-  LOGVAR+=$(echo -e "Backup complete at $EDATETIME" | tee -a $DESDIR/$LOGFILENAME)'\n'
-  LOGVAR+=$(echo -e "Backup duration: $DURATION" | tee -a $DESDIR/$LOGFILENAME)'\n'
-  LOGVAR+=$(echo -e "$HOST backup completed successfully" | tee -a $DESDIR/$LOGFILENAME)'\n'
-  BACKUPSIZE=$(stat -c '%s' $DESDIR/$FILENAME | numfmt --to=iec --suffix=B --format='%3.3f')
-  LOGVAR+=$(echo -e "Backup size: $BACKUPSIZE")'\n\n'
-  LOGVAR+=$(echo -e "$DESDIR contents:")'\n'
-  LOGVAR+=$(echo -e "$DESDIRLS")'\n'
-  echo -e "$LOGVAR" | mail -s "$HOST Backup success - $DATE" $EMAIL
+##### Load config
+if [ -f "config.sh" ]; then
+  . config.sh
 else
-  # MOUNT not mounted - send email
-  echo "$MOUNT not mounted on $HOST" | mail -s "$HOST Backup failed - $DATE" $EMAIL
+  echo "config.sh missing"
 fi
+SLBVER="~~ sage-linux-backup version 1.5 July 2020 ~~"
+
+##### Functions
+
+backup()
+{
+  if grep -qs "$MOUNT" /proc/mounts; then
+    # MOUNT mounted - proceed with backup
+    LOGVAR+=$(echo -e "Backup started at $SDATETIME" | tee -a $DESDIR/$LOGFILENAME)'\n'
+    LOGVAR+=$(echo -e "Backup $HOST $SRCDIR to $DESDIR" | tee -a $DESDIR/$LOGFILENAME)'\n'
+    # run backup
+    ##### tar -cpzvf $DESDIR/$FILENAME --directory=$SRCDIR --exclude-from $EXCLIST . &>> $DESDIR/$LOGFILENAME
+    EDATETIME=`date "+%Y-%m-%d %H:%M:%S"`             # get date and time
+    DURATION=`dateutils.ddiff -f "%H hours and %M minutes and %S seconds" "$SDATETIME" "$EDATETIME"`
+    DESDIRLS=`find $DESDIR/* -maxdepth 1 -printf '%CY-%Cm-%Cd - %f  \t- %s\n' | numfmt --to=iec --suffix=B --field 5 --format='%3.3f' | sed 's/.000//'`
+    LOGVAR+=$(echo -e "Backup complete at $EDATETIME" | tee -a $DESDIR/$LOGFILENAME)'\n'
+    LOGVAR+=$(echo -e "Backup duration: $DURATION" | tee -a $DESDIR/$LOGFILENAME)'\n'
+    LOGVAR+=$(echo -e "$HOST backup completed successfully" | tee -a $DESDIR/$LOGFILENAME)'\n'
+    BACKUPSIZE=$(stat -c '%s' $DESDIR/$FILENAME | numfmt --to=iec --suffix=B --format='%3.3f')
+    LOGVAR+=$(echo -e "Backup size: $BACKUPSIZE")'\n\n'
+    LOGVAR+=$(echo -e "$DESDIR contents:")'\n'
+    LOGVAR+=$(echo -e "$DESDIRLS")'\n\n'
+    LOGVAR+=$(echo -e "$SLBVER")'\n'
+    echo -e "$LOGVAR"
+    ##### echo -e "$LOGVAR" | mail -s "$HOST Backup success - $DATE" $EMAIL
+  else
+    # MOUNT not mounted - send email
+    echo "$MOUNT not mounted on $HOST"
+    ##### echo "$MOUNT not mounted on $HOST" | mail -s "$HOST Backup failed - $DATE" $EMAIL
+  fi
+
+} # end of backup
+
+cleanup()
+{
+  DESDIR=DESDIR/*
+
+  if grep -qs "$MOUNT" /proc/mounts; then
+    # MOUNT mounted - proceed with backup cleanup
+    find $DESDIR -mtime +$NDAYS -type f -delete
+  else
+    mount "$MOUNT"
+    if [ $? -eq 0 ]; then
+      # MOUNT was mounted successfully
+      find $DESDIR -mtime +$NDAYS -type f -delete
+    else
+      # MOUNT not mounted - send email
+      echo "$MOUNT not mounted on $HOST" | mail -s "$HOST Backup cleanup failed - $DATETIME" sixfootscott@gmail.com
+    fi
+  fi
+
+} # end of cleanup
+
+usage()
+{
+  echo "usage: sage-linux-backup.sh [[[-b backup ] | [-c cleanup] | [-h]]"
+}
+
+##### Main
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --backup )         backup
+                                exit
+                                ;;
+        -c | --cleanup )        cleanup
+                                exit
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
 
 #END
